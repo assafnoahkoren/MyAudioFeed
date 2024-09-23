3487b390f233d7cd6514de3d6b1c3aeb444124d6
Assaf Koren <assafnoahkoren@gmail.com>
2024-09-23 22:51:42 +0300

    .

--- webapp/src/queries/jobs/get.ts
+++ webapp/src/queries/jobs/get.ts
@@ -19,6 +19,7 @@ export const useMyJobsQuery = () => {
             event: '*',
             schema: 'public',
             table: 'jobs',
+            filter: `owner_id=eq.${user?.id}`,
             enabled: !!user
         },
         enabled: !!user
